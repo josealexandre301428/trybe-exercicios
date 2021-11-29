26b2e98945bf0e59acd469b28113702ed9655f06
josealexandre301428 <josealexandre301428@gmail.com>
2021-11-29 15:10:47 -0300

    adiciona 3 exercicios de array do dia 2 do JS

---
+++ Fundamentos/B4-Introducao-Js-/exercicio4.1.js
@@ -0,0 +1,88 @@
+/*const a = 5;
+const b = 10;*/
+
+
+/* Adição */
+
+
+/*console.log(a + b);*/
+
+/* Subtração */
+
+
+/*console.log(a - b);*/
+
+/* Multiplicação */
+
+ 
+/*console.log(a * b);*/
+
+/* Divisão */
+
+
+/*console.log(a / b);*/
+
+
+/* Modular */
+
+/*console.log(a % b === 0);*/
+
+
+
+
+
+
+/* ----------------2 parte mostrar maior dos numeros-------------- */
+/*const a = 5;
+const b = 10;
+
+if (a > b){
+  console.log(a);
+} else{
+  console.log(b);
+}
+*/
+
+/* -------------------3 parte maior de tres numeros---------------- */
+
+/*const a = 5;
+const b = 10;
+const c = 15;
+
+
+if(a > b || a > c){
+  console.log(a);
+} else if(b > a && b>c){
+  console.log(b);
+}else{
+console.log(c);
+}*/
+
+
+/* ------------------4 parte positive----------------------------- */
+
+
+/*const num=10;
+
+if(num % 2 === 0){
+  console.log("Positivo");
+}
+else if(num % 2 !== 0){
+  console.log("Negativo");
+} else{
+  console.log('zero');
+}*/
+
+
+/* ------------------5 parte--------------------------------------- */
+
+
+const a;
+const b;
+const c;
+
+a=45;
+b=90;
+c=45;
+
+
---
+++ Fundamentos/B4-Introducao-Js-/exercicio4.2.js
@@ -0,0 +1,26 @@
+
+/* 1 exercicio */
+
+/*let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);*/
+
+
+/* 2 exercicio */
+
+/*let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let indexOfPortifolio = menu.indexOf('Portifólio');
+
+console.log(indexOfPortifolio);*/
+
+/* 3 exercicio */
+
+/*let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+
+menu.push('Contato');
+
+console.log(menu);*/
+
+
+/* exercicios array concluidos */
