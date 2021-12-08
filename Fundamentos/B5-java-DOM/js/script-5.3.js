16de2f31b7aaf75b1a85e5a8ad2dd4fa1634faf0
josealexandre301428 <josealexandre301428@gmail.com>
2021-12-08 03:07:10 -0300

    adiciona exercicios de fixaçao ao repositorio

--- Fundamentos/B5-java-DOM/js/script-5.3.js
+++ Fundamentos/B5-java-DOM/js/script-5.3.js
@@ -1,39 +1,125 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
-const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let ulDays = document.getElementById("days");
+createDays();
+function createDays(){
+  for(let index in dezDaysList){
+    let dias = document.createElement('li');
+    dias.className = "day";
+    if(dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31){
+      dias.className += " holiday"
+    }
+
+    if(dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25){
+      dias.className += " friday"
+    }
+
+    dias.innerText = dezDaysList[index];
+    ulDays.appendChild(dias);
+  }
+}
+function createHolidayButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  let newButtonID = 'btn-holiday';
+
+  newButton.innerHTML = buttonName;
+  newButton.id = newButtonID;
+  buttonContainer.appendChild(newButton);
+};
+
+createHolidayButton('Feriados');
+
+function displayFridays(fridaysArray) {
+  let getFridayButton = document.getElementById('btn-holiday');
+  let fridays = document.getElementsByClassName("friday");
+  let newFridayText = 'SEXTOU o/';
+
+  getFridayButton.addEventListener('click', function() {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+      }
+    }
+  })
+};
+
+let dezFridays = [ 4, 11, 18, 25 ];
+displayFridays(dezFridays);
 
 
-// 1. Copie esse arquivo e edite apenas ele;
-// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
-// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
-
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portifólio?
-
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
-
-// Segue abaixo um exemplo do uso de event.target:
+function dayZoom (){
+let days = document.querySelector('#days');
 
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+days.addEventListener("mouseover", function(event){
+  event.target.style.fontSize = "40px";
+  event.target.style.fontWeigth = '600';
+})
+};
+
+function dayMouseOut () {
+  let days = document.querySelector('#days');
+  days.addEventListener("mouseout", function(event){
+    event.target.style.fontSize = "20px";
+    event.target.style.fontWeigth = '200';
+  })
+};
+
+dayZoom();
+dayMouseOut();
+
+
+function myTask(task){
+  let taskContaint = document.querySelector('.my-tasks');
+  let taskName = document.createElement('span');
+
+  taskName.innerHTML = task;
+  taskContaint.appendChild(taskName);
+
 }
 
-firstLi.addEventListener('dblclick', resetText);
+myTask("Projeto:");
 
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+function newTaskDiv(color){
+  let taskContainer = document.querySelector('.my-tasks');
+  let newTask = document.createElement('div');
+
+  newTask.className = "task";
+  newTask.style.backgroundColor = color;
+  taskContainer.appendChild(newTask);
+}
+
+newTaskDiv("green");
+
+function selectDiv(){
+  let selectedTask = document.getElementsByClassName('task selected');
+  let myTasks = document.querySelector('.task');
+
+  myTasks.addEventListener('click', function(event){
+    if(selectedTask.length === 0){
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  });
+};
+
+selectDiv();
