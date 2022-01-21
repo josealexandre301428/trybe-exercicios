76295a18edb1c8831533a065ce3a416066010b8d
josealexandre301428 <josealexandre301428@gmail.com>
2022-01-21 16:44:00 -0300

    exercicios do dia 4

--- Fundamentos/B8-Hofs/exercice2-D2.js
+++ Fundamentos/B8-Hofs/exercice2-D2.js
@@ -61,4 +61,59 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+// exercicio 1
+/*const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];*/
+
+/*const formatedBookNames = (books) => {
+  let formate = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+  return formate;
+}
+console.log(formatedBookNames(books));*/
+
+// exercicio 2
+/*
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];*/
+
+function nameAndAge(books) {
+  let objeto;
+  let map = books.map((book) => {
+    objeto.age = book.age;
+    objeto.author = book.author;
+  });
+  /*let sort = map.sort(() =>)*/
+};
+
+  
+console.log(nameAndAge(books));
