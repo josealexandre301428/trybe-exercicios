b24d10dba323482f09aebd81e3698f62019b9ecd
josealexandre301428 <josealexandre301428@gmail.com>
2022-02-16 20:07:22 -0300

    adiciona exercicios 2 e 3

---
+++ Front-End/Bloco-1-introducao/ex3d1/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class footer extends React.Component {
+  render() {
+    return(
+      <div className='footer'>
+        <h3>E isso é só o começo...</h3>
+      </div>
+    );
+  }
+}
+
+export default footer;
---
+++ Front-End/Bloco-1-introducao/ex3d1/src/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class header extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1 className='App-header'>Conteúdos de Front-End</h1>
+      </div>
+    );
+  }
+}
+
+export default header;
