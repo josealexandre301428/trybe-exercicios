6ed3858d268d0da604b67969ac80c80a2fc12cd4
josealexandre301428 <josealexandre301428@gmail.com>
2022-02-16 18:28:50 -0300

    atualiza

--- Front-End/Bloco-1-introducao/nome-app/src/App.js
+++ Front-End/Bloco-1-introducao/nome-app/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
