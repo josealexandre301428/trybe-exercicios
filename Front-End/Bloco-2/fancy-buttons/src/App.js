f73b9d4b1a068297afd721d725f234d3d8ffdbf9
josealexandre301428 <josealexandre301428@gmail.com>
2022-02-22 09:33:54 -0300

    feat: realiza exercicios de fixaçao

--- Front-End/Bloco-2/fancy-buttons/src/App.js
+++ Front-End/Bloco-2/fancy-buttons/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numeroDeCliques: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(){
+    const botao = document.querySelector('#botao');
+    this.setState((anterior, _props) => ({
+      numeroDeCliques: anterior.numeroDeCliques + 1
+    }))
+    botao.className = 'color';
+    if (this.state.numeroDeCliques % 2 !== 0 ) {
+      botao.className = 'colorGreen';
+    } else {
+    botao.className = 'color';
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <button id='botao' onClick={this.handleClick}>Meu Botão!{this.state.numeroDeCliques}</button>
+      </div>
+    )
+  }
+
+  
 }
 
 export default App;
