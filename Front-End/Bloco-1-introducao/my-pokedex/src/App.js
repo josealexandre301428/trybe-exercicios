472d2b1f91a23277fbb1f07e89ffac659baf764f
josealexandre301428 <josealexandre301428@gmail.com>
2022-02-22 15:40:19 -0300

    realiza exercicio do bloco 11(estado)

--- Front-End/Bloco-1-introducao/my-pokedex/src/App.js
+++ Front-End/Bloco-1-introducao/my-pokedex/src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.css';
 import pokemons from './Data';
+import Data from './Data';
 import Pokedex from './Pokedex';
 
 
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      indexDoPokemon: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    if (this.state.indexDoPokemon < 8) {
+      this.setState((anterior, _props) => ({
+        indexDoPokemon: anterior.indexDoPokemon + 1
+      }))
+    } else {
+      this.setState({
+        indexDoPokemon: 0
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='title'> Pokedex </h1>
-        <Pokedex pokemons={pokemons} />
+        <section className='main'>
+          <Pokedex pokemons={Data[this.state.indexDoPokemon]} />
+          <button className='btn' onClick={this.handleClick} >Next!</button>
+        </section>
+       
     </div>
   );
 }
--- Front-End/Bloco-1-introducao/my-pokedex/src/Pokedex.js
+++ Front-End/Bloco-1-introducao/my-pokedex/src/Pokedex.js
@@ -3,10 +3,10 @@ import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
   render() {
-    const { pokemons  } = this.props;
+    const { pokemons } = this.props;
     return (
       <div className="pokedex">
-        {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        <Pokemon pokemon={pokemons} />
       </div>
     
     )
