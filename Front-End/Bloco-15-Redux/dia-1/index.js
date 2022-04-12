237bca61baaa68b38b4c4728e1ea7b783adda9eb
José Alexandre Filho <josealexandre301428@gmail.com>
2022-04-12 15:01:14 -0300

    reorganiza repositorio local

---
+++ Front-End/Bloco-15-Redux/dia-1/index.js
@@ -0,0 +1,12 @@
+const ESTADO_INICIAL = {
+  login: false,
+  email: "",
+};
+
+const reducer = (state = ESTADO_INICIAL) => {
+  return state;
+};
+
+const store = Redux.createStore(reducer);
+
+console.log(store.getState());
